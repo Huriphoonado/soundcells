f46a1a1bad249daa6c1cbf482a85310058d1fb85
Willie Payne <williepain@gmail.com>
2021-07-09 13:46:54 -0400

    Begin project
    initialized project and installed some basic dependencies

---
+++ README.md
@@ -0,0 +1,10 @@
+# Sound Cells
+
+## Setup
+This app uses a virtual environment `venv` to ensure that. So to use, you'll need to create a virtual environment and then install all the dependencies listed in requirements.txt.
+
+```bash
+$ python3 -m venv venv
+$ . venv/bin/activate
+$ pip install -r requirements.txt
+```
---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const terserOpts = {
+    compress: {ecma: 2015, module: true},
+    mangle: {module: true},
+    output: {ecma: 2015},
+    parse: {ecma: 2015},
+    rename: {},
+}
+
+export default (async () => ({
+    output: {
+        format: 'es',
+        sourcemap: true,
+        entryFileNames: '[name].[hash].js',
+    },
+    plugins: [
+        resolve(),
+        commonjs(),
+        isProduction && (await import('rollup-plugin-terser')).terser(terserOpts),
+    ]
+}))();
